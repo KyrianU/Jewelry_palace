04da6eef3b6238514e7844a35044db260b4b42a5
Kyrian Uwaegbulem <kyrian.uwaegbulem@gmail.com>
2025-02-16 13:28:49 +0000

    shopping bag template updated

--- jewelry_palace/settings.py
+++ jewelry_palace/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=fs%r4nf%615juz1mkur0g(&basnx*$d4-61-^s_*v=%((z$(e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
