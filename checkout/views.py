3de61d84b385f78cdca6a4ed918f2e7650201a63
Kyrian Uwaegbulem <kyrian.uwaegbulem@gmail.com>
2025-03-05 23:03:58 +0000

    added checkout form data in payemnt intent

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404, HttpResponse
+)
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 
 from .forms import OrderForm
@@ -8,6 +11,24 @@ from bag.contexts import bag_contents
 from django.conf import settings
 
 import stripe
+import json
+
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
@@ -26,6 +47,7 @@ def checkout(request):
             'town_or_city': request.POST['town_or_city'],
             'street_address1': request.POST['street_address1'],
             'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
@@ -37,7 +59,7 @@ def checkout(request):
                         order_line_item = OrderLineItem(
                             order=order,
                             product=product,
-                            quanity=item_data,
+                            quantity=item_data,
                         )
                         order_line_item.save()
                     else:
@@ -56,7 +78,7 @@ def checkout(request):
                     )
                     order.delete()
                     return redirect(reverse('view_bag'))
-            request.session['save_info'] = 'save_info' in request.POST
+            request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
             messages.error(request, 'There was an error with your form. \
@@ -100,7 +122,7 @@ def checkout_success(request, order_number):
     save_info = request.session.get('save_info')
 
     order = get_object_or_404(Order, order_number=order_number)
-    messages.success(request, f'Order succesfully processed! \
+    messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation  \
         email will be sent to {order.email}.')
 
