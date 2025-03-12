0732e537072786b9dbc58608d59d43ed5c1d4c18
Kyrian Uwaegbulem <kyrian.uwaegbulem@gmail.com>
2025-03-12 23:26:12 +0000

    added privacy policy page

--- jewelry_palace/urls.py
+++ jewelry_palace/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,5 +26,8 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
+    path('privacy/',
+         TemplateView.as_view(template_name='privacy.html'),
+         name='privacy'),
     path('profiles/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
