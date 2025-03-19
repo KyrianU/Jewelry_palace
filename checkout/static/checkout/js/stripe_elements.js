cdaa7187ea1442f4064e8d926b2848a10ab95c2b
Kyrian Uwaegbulem <kyrian.uwaegbulem@gmail.com>
2025-03-19 11:48:43 +0000

    fixed indentation issue on my checkout models.py file

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -56,7 +56,7 @@ form.addEventListener('submit', function(ev) {
         'save_info': saveInfo,
     };
     var url = '/checkout/cache_checkout_data/';
-console.log("Email value:", form.email.value)
+
     $.post(url, postData).done(function() {
         stripe.confirmCardPayment(clientSecret, {
             payment_method: {
