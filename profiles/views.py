f6e6f88f459b44982b9e0e5c24be5819c8ec55fd
Kyrian Uwaegbulem <kyrian.uwaegbulem@gmail.com>
2025-06-26 20:31:32 +0100

    fixed bug that was not allowing to display order history

--- profiles/views.py
+++ profiles/views.py
@@ -51,7 +51,7 @@ def order_history(request, order_number):
     template = "checkout/checkout_success.html"
     context = {
         "order": order,
-        "form_profile": True,
+        "from_profile": True,
     }
 
     return render(request, template, context)
